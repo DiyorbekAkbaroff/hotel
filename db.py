cc6d863086b255c888ddfda06d62bc3d1098f715
djumanov <djumanovdev@gmail.com>
2025-02-19 15:14:10 +0500

    db

---
+++ db.py
@@ -0,0 +1,62 @@
+import hashlib
+import mysql.connector
+import config
+
+
+class DB:
+    def __init__(self):
+        self.__connection = mysql.connector.connect(
+            host=config.DB_HOST,
+            port=config.DB_PORT,
+            user=config.DB_USER,
+            password=config.DB_PASS,
+            database=config.DB_NAME
+        )
+        self.__cursor = self.__connection.cursor()
+
+    def has_connection(self) -> bool:
+        return self.__connection.is_connected()
+
+    def __is_user(self, username: str) -> bool:
+        self.__cursor.execute("SELECT * FROM users WHERE username = %s", (username,))
+        users = self.__cursor.fetchall()
+
+        if users:
+            return True
+        else:
+            return False
+
+    def create_user(self, name: str, username: str, email: str, password: str) -> bool:
+        if self.__is_user(username):
+            return False
+        else:
+            hashed_pass = hashlib.sha256(password.encode()).hexdigest()
+
+            self.__cursor.execute("""INSERT INTO users (name, username, email, password)
+                VALUES (%s, %s, %s, %s)
+            """, (name, username, email, hashed_pass))
+
+            self.__commit()
+
+            return True
+
+    def check_user(self, username: str, password: str) -> int | bool:
+        hashed_pass = hashlib.sha256(password.encode()).hexdigest()
+
+        self.__cursor.execute("SELECT * FROM users WHERE username = %s AND password = %s", (username, hashed_pass))
+        users = self.__cursor.fetchall()
+
+        if users:
+            return users[0]
+        else:
+            return False
+
+    def __commit(self) -> None:
+        self.__connection.commit()
+
+    def __close(self) -> None:
+        self.__cursor.close()
+        self.__connection.close()
+
+            
+
---
+++ test.py
@@ -0,0 +1,6 @@
+from db import DB
+
+
+db = DB()
+
+print(db.create_user('Vali Aliyev', 'vali', 'vali@gmail.com', '456'))
