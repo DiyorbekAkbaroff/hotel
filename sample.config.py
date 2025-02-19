2375545df130a19bf83774b7dac3fa711d8d93d9
djumanov <djumanovdev@gmail.com>
2025-02-19 15:16:30 +0500

    exmaple for config.py

---
+++ sample.config.py
@@ -0,0 +1,11 @@
+# SERVER
+HOST = 'server host'
+PORT = 'server port'
+DEBUG = 'debug mode'
+
+# DATABASE
+DB_HOST = 'db host'
+DB_PORT = 'db port'
+DB_USER = 'db user'
+DB_PASS = 'db password'
+DB_NAME = 'db name'
